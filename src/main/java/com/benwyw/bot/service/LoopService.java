ff74fece62e5cf403e4d17971a809e034784a8bd
Ben <57297093+Benwyw@users.noreply.github.com>
2023-03-05 00:13:01 +0800

    Add LoopService.java

---
+++ src/main/java/com/benwyw/bot/service/LoopService.java
@@ -0,0 +1,12 @@
+package com.benwyw.bot.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class LoopService {
+	/**
+	 * TODO Migrate loop-based service
+	 */
+}
