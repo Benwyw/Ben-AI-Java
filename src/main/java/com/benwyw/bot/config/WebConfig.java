d890c141b10df82a1f6bf2fc1cc14e041e5ba58d
Ben <57297093+Benwyw@users.noreply.github.com>
2023-03-10 22:47:40 +0800

    Allow domain port Up version v0.0.15

--- src/main/java/com/benwyw/bot/config/WebConfig.java
+++ src/main/java/com/benwyw/bot/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*", "http://bot.benwyw.com")
+                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*", "http://bot.benwyw.com:*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
