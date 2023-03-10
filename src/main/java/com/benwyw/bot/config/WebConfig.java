0e2651457ce76cf5ba52962b45f3d90b4d2ae86f
Ben <57297093+Benwyw@users.noreply.github.com>
2023-03-10 23:33:34 +0800

    HTTPS but self-signed for now Up version v0.0.16

--- src/main/java/com/benwyw/bot/config/WebConfig.java
+++ src/main/java/com/benwyw/bot/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*", "http://bot.benwyw.com:*")
+                .allowedOriginPatterns("https://localhost:*", "https://127.0.0.1:*", "https://bot.benwyw.com", "https://bot.benwyw.com:*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
