6994e9ec165bdd23fd8f6ff82e83c6752b1b409a
Ben <57297093+Benwyw@users.noreply.github.com>
2025-08-10 23:50:44 +0800

    Refactor AuthFilter to remove unused imports and clean up code

--- src/main/java/com/benwyw/bot/config/security/AuthFilter.java
+++ src/main/java/com/benwyw/bot/config/security/AuthFilter.java
@@ -13,9 +13,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
